6dd6873caf0de79103522b69f695e5df7314a8ee
clintjeff2 <clintjeff2@gmail.com>
2025-06-03 19:43:54 +0100

    Fixed broken imported for prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'; 
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
-import { User } from './userAuth/dto/create-user.dto';
 import { UserProfileService } from './userAuth/user.profile.service';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { JwtAuthGuard } from './userAuth/guards/jwt-auth.guard';
 import { UserProfileController } from './userAuth/user.profile.controller';
 import { AuthController } from './userAuth/auth.controller';
@@ -14,40 +13,42 @@ import { OtpService } from './userAuth/otp.service';
 import { ToolModule } from './Tools/tool.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    JwtModule.register({         
-      secret: process.env.JWT_SECRET || 'default_secret',
-      signOptions: { expiresIn: '1d' },
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT) || 5432,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      entities: [User],
-      migrations: ['src/migrations/*.ts'],
-      synchronize: true,
-      ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
-      extra: {
-        ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
-      },
-    }),
-    ToolModule,
-  ],
-  controllers: [UserProfileController, AuthController],
-  providers: [
-    MailService,
-    UserProfileService,
-    PrismaService,
-    OtpService,
-    JwtAuthGuard,
-    AuthService,
-  ],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		JwtModule.register({
+			secret: process.env.JWT_SECRET || 'default_secret',
+			signOptions: { expiresIn: '1d' },
+		}),
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: process.env.DB_HOST,
+			port: Number(process.env.DB_PORT) || 5432,
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
+			autoLoadEntities: true,
+			entities: [__dirname + '/**/*.entity{.ts}'],
+			migrations: ['src/migrations/*.ts'],
+			synchronize: true,
+			ssl:
+				process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+			extra: {
+				ssl:
+					process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+			},
+		}),
+		ToolModule,
+	],
+	controllers: [UserProfileController, AuthController],
+	providers: [
+		MailService,
+		UserProfileService,
+		PrismaService,
+		OtpService,
+		JwtAuthGuard,
+		AuthService,
+	],
 })
 export class AppModule {}
