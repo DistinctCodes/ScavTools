ba5501542d331c9ee01b1bb8f663d89e542894a9
clintjeff2 <clintjeff2@gmail.com>
2025-06-04 19:42:42 +0100

    Update Auth module: Remove TypeORM imports, keep Prisma service

--- backend/src/userAuth/auth.module.ts
+++ backend/src/userAuth/auth.module.ts
@@ -2,15 +2,20 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { MailService } from './mail.service';
+import { OtpService } from './otp.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: 'ACCESS_SECRET',
-      signOptions: { expiresIn: '15m' },
-    }),
-  ],
-  providers: [AuthService, JwtStrategy],
-  exports: [AuthService],
+	imports: [
+		JwtModule.register({
+			secret: 'ACCESS_SECRET',
+			signOptions: { expiresIn: '15m' },
+		}),
+	],
+	controllers: [AuthController],
+	providers: [AuthService, JwtStrategy, MailService, OtpService, PrismaService],
+	exports: [AuthService],
 })
 export class AuthModule {}
