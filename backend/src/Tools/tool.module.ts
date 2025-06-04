456f2e0c9cb94ccbf548ec7b229611b7baa58d9a
clintjeff2 <clintjeff2@gmail.com>
2025-06-04 19:42:37 +0100

    Update Tool module: Remove TypeORM imports, add Prisma dependencies

--- backend/src/Tools/tool.module.ts
+++ backend/src/Tools/tool.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tool } from './entities/tool.entity';
 import { ToolController } from './tool.controller';
 import { ToolService } from './tool.service';
-import { ToolAccessLog } from '../Tools/entities/tool-access-log.entity';
-import { ToolAccessLogService } from '../Tools/tool-access-log.service';
-import { User } from 'src/userAuth/entities/user.entity';
+import { ToolAccessLogService } from './tool-access-log.service';
+import { PrismaService } from './../../prisma/prisma.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Tool, ToolAccessLog, User]),
-  ],
-  controllers: [ToolController],
-  providers: [ToolService, ToolAccessLogService],
+	controllers: [ToolController],
+	providers: [ToolService, ToolAccessLogService, PrismaService],
+	exports: [ToolService, ToolAccessLogService],
 })
 export class ToolModule {}
